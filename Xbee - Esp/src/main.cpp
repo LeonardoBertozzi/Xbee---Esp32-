65204bb8325cfe94655d20a6a2102fae480eeaf4
LeonardoBertozzi <58674240+LeonardoBertozzi@users.noreply.github.com>
2022-12-29 09:49:27 -0300

    Add files via upload
    Atualizada a conversão de strings para hexadecimal, feito as indexações do cabeçalho "0x", atualmente falta a conversão de string ou char para byte, que é o formato enviado para o xbee.

--- Xbee - Esp/src/main.cpp
+++ Xbee - Esp/src/main.cpp
@@ -11,24 +11,34 @@
 
 String BuildPackage(String payload, int len)
 {
-  char index[50] = "0x";
-  char delimiter[] = ",";
-  char char_array[50];
-  char teste[50];
-  
-  payload.toCharArray(char_array,len);
-  char* success = strdup(char_array);
-  free(success);
+  String Payload1;
+  String Payload2;
+  char teste[len];
+  int lenght = (payload.length()+1)/2;
+  int j = 0;
 
-  for(int i = 0; i < strlen(success); i++){
-    //Serial.printf("%c", success[i]);
-    //Serial.printf("/");
-    Serial.println(i);
-    i++;
-    Serial.println("/-/");
-    Serial.println(i);
+  Serial.println();
+  for(int i = 0; i < lenght; i++){
+    Payload1 = payload.substring(j,j+=2);
+    if( i == (lenght -1))
+    {
+      Payload1 = "0x" + Payload1 ;
+    }
+    else
+    {
+      Payload1 = "0x" + Payload1 + ",";
+    }
+    
+    //Serial.print(Payload1);
+    Payload2 = Payload2 + Payload1;
   }
-  return char_array;
+  //Serial.println(Payload2);
+  Payload2.toCharArray(teste,len);
+  Serial.println(teste);
+  byte input2 = (byte)atoi(teste);  
+  Serial.println(input2);
+
+  return Payload1;
 }
 
 String StringToHex(char* input)
@@ -41,29 +51,26 @@ String StringToHex(char* input)
   int len = strlen(input);
   char output[(len*2)+1];
 
-  
-  //strncat(index,hex_str,2);
-  //strcpy(str2,index);
-  //strncat(str2,delimiter,2);
-  //Serial.println(index);
-  //Serial.println(str2);
-
   while(input[loop] != '\0')
   {
-    sprintf((char*)(output+i),"%02X", input[loop]);
+    sprintf((char*)(output+i),"%02x", input[loop]);
         loop+=1;
         i+=2;
   }
+//Serial.println(output);
 String package = output;
-BuildPackage(package,(len*4)+1);
+//Serial.println(package);
+BuildPackage(package,(len*5));
 return package;
 }
 
 
 
 byte packet [] = {0x7E, 0x00, 0x1B, 0x10, 0x01, 0x00, 0x7D, 0x33, 0xA2, 0x00, 0x41, 0xD9, 0x8B, 0x5F, 0xFF, 0xFE, 0x00, 0x00, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x20, 0x21, 0xDB};
+String pack = {"0x7E, 0x00, 0x1B, 0x10, 0x01, 0x00, 0x7D, 0x33, 0xA2, 0x00, 0x41, 0xD9, 0x8B, 0x5F, 0xFF, 0xFE, 0x00, 0x00, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x20, 0x21, 0xDB"};
 
 void setup() {
+  // put your setup code here, to run once:
   
   Serial.begin(9600);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
@@ -72,8 +79,11 @@ void setup() {
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.println(packet);
+  
   Serial2.write(packet, sizeof(packet));
-  char ascii_str [] = "Hello World!";;
+  char ascii_str [] = "Hello World!";
   (StringToHex(ascii_str));
   delay(5000);
 }
